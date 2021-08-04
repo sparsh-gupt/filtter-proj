93ce68757f33b09a1146783dc4e85c737677fa97
sparsh-gupt <72927084+sparsh-gupt@users.noreply.github.com>
2021-08-04 17:44:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas= createCanvas(400,400);
+    canvas.center();
+}
+
+function draw() {
+    
+}
+
+function take_snapshot() {
+    save('your_filtered_image.jpg');
+}
